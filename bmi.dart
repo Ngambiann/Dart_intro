ef30ceea301bea96da4b52a49bc0173344880a5d
ann-ngambi <ngambiann08@gmail.com>
2024-08-02 16:46:42 +0300

    sphere volume

--- bmi.dart
+++ bmi.dart
@@ -2,20 +2,25 @@ import 'dart:io';
 import 'dart:math';
 
 void main(){
-  print("what is your name?");
+  print("Enter your name?");
   name=(stdin.readLineSync());
   print("Enter your weight in kg:");
   weight=double.parse(stdin.readLineSync()!);
   print("Enter your height in metres:");
   height=double.parse(stdin.readLineSync()!);
-  print("Your BMI is:${calculateBmi()}");
+  print("Your BMI is:${calculateBmi(height,weight)}");
 
   }
 String? name;
 double? weight;
 double? height;
 const int exponent=2;
-double calculateBmi(){
-  double Bmi=weight! / pow(height!, exponent);
+double calculateBmi(height,weight){
+  double Bmi=weight / pow(height, exponent);
   return Bmi;
 }
+/*double calculateBmi(){
+  double Bmi=weight! / pow(height!, exponent);
+  return Bmi;
+  --null acceptor=!
+  */
