ad2496bf8c17cf461a5f737651f8a29f3c62d00d
ann-ngambi <ngambiann08@gmail.com>
2024-08-05 12:06:48 +0300

    collections

--- loops.dart
+++ loops.dart
@@ -1,15 +1,33 @@
+
+
 List <int> numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
 
 //for(initialization;condition;afterThought){implementation}
-void printAllArray(){
+/*void printAllArray(){
 //numbers.forEach(print);
 for( int i in numbers ){
   if(i >10)
   print("$i is greater than 10");
 
-}
-}
+}*/
+
+//COLLECTIONS:
+List colors=["Blue","Grey","Yellow","Red"];
+
+ void printcolors(){
+colors.forEach(print);
+colors.add("Green");
+colors.forEach(print);
+ }
+
+
+
+
+
+
 void main(){
-  printAllArray();
+  //printAllArray();
+  printcolors();
+  
 }
